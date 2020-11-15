803989e4d1c2a71f9085d42d1a38ce759d99d053
McXD <74167137+McXD@users.noreply.github.com>
2020-11-15 16:09:48 +0800

    Update Application.java

--- Comp2021Project/src/hk/edu/polyu/comp/comp2021/g17/cvfs/Application.java
+++ Comp2021Project/src/hk/edu/polyu/comp/comp2021/g17/cvfs/Application.java
@@ -1,5 +1,9 @@
 package hk.edu.polyu.comp.comp2021.g17.cvfs;
 
-public class Application {
+import hk.edu.polyu.comp.comp2021.g17.cvfs.model.filesystem.FileSystem;
 
+public class Application {
+	public static void main(String[] args) {
+		new FileSystem();
+	}
 }
