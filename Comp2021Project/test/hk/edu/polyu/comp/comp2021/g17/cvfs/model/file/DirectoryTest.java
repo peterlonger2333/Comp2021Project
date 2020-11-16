376eb6589cee81051030316149d6ebe5f55d0c69
McXD <74167137+McXD@users.noreply.github.com>
2020-11-16 11:52:35 +0800

    Update DirectoryTest.java

--- Comp2021Project/test/hk/edu/polyu/comp/comp2021/g17/cvfs/model/file/DirectoryTest.java
+++ Comp2021Project/test/hk/edu/polyu/comp/comp2021/g17/cvfs/model/file/DirectoryTest.java
@@ -20,25 +20,37 @@ class DirectoryTest {
 	@BeforeAll
 	static void constructorAndToStringTest() throws InvalidFileNameException {
 		Directory testDir1 = new Directory("testDir1",null);
-		Directory testDir2 = new Directory("testDir2",testDir1);
 		assertEquals("[.]", testDir1.toString(), "Directory should be initialized.");
+		Directory testDir2 = new Directory("testDir2",testDir1);
 		assertEquals("[.,..]", testDir2.toString(), "Directory should be initialized.");
+		assertEquals("[.,testDir2]", testDir1.toString(), "Parent directory should detect changes (in dirent)");
 	}
 	
 	@BeforeEach
-	void init() throws InvalidFileNameException {
-		d0 = new Directory("/",null);
-		d1 = new Directory("dir1",d0);
-		d2 = new Directory("dir2",d1);
+	void init(){
+		try {
+			d0 = new Directory("/",null);
+			d1 = new Directory("dir1",d0);
+			d2 = new Directory("dir2",d1);
+		}catch(Exception e) {
+			assertEquals(1,2,"No exception shall be thrown");
+		}
+		
+
 	}
 	
 	@Test
 	@Order(1)
-	void newDirTest() throws InvalidFileNameException, FileAlreadyExistException {
-		int oriSize = d0.getSize();
-		d2.newDir("testDir2");
-		assertEquals("[.,..,testDir2]",d2.toString(),"Directory should contain desired contents");
-		assertEquals(oriSize + 40, d0.getSize(), "Sizes of parent directories should detect the change");
+	void newDirTest() {
+		try {
+			int oriSize = d0.getSize();
+			d2.newDir("testDir2");
+			assertEquals("[.,..,testDir2]",d2.toString(),"Directory should contain desired contents");
+			assertEquals(oriSize + 40, d0.getSize(), "Sizes of parent directories should detect the change");
+		}catch(Exception e) {
+			assertEquals(1,2,"No exception shall be thrown");
+		}
+		
 	}
 	
 	@Test
@@ -55,24 +67,27 @@ class DirectoryTest {
 			assertThrows(FileAlreadyExistException.class, () -> d2.newDoc("testDoc","some other content",DocumentType.txt),
 					"Cannot hava documents with same in one directory");
 		}catch(Exception e) {
-			e.printStackTrace();
+			assertEquals(1,2,"No exception shall be thrown");
 		}
 	}
 	
 	@Test
 	@Order(3)
-	void deleteTest() throws FileAlreadyExistException, InvalidFileNameException, IllegalOperationException, FileNotExistException {
-		Document testDoc = new Document("testDoc", DocumentType.txt, "testDocContent");
-		int d0InitSize = d0.getSize();
-		d2.newDoc(testDoc.name, (String) testDoc.content, testDoc.getDocumentType());
-		d2.delete("testDoc");
-		
-		assertEquals("[.,..]",d2.toString(),"File should be deleted");
-		assertEquals(d0InitSize, d0.getSize(), "Sizes should be adjusted");
-		assertThrows(FileNotExistException.class, () -> d2.delete("testDoc"), "Cannot delete a file that does not exist");
-		assertThrows(IllegalOperationException.class, () -> d2.delete("."), "Cannot delete '.'");
-		assertThrows(IllegalOperationException.class, () -> d2.delete(".."), "Cannot delete '..'");
-
+	void deleteTest(){
+		try {
+			Document testDoc = new Document("testDoc", DocumentType.txt, "testDocContent");
+			int d0InitSize = d0.getSize();
+			d2.newDoc(testDoc.name, (String) testDoc.content, testDoc.getDocumentType());
+			d2.delete("testDoc");
+			
+			assertEquals("[.,..]",d2.toString(),"File should be deleted");
+			assertEquals(d0InitSize, d0.getSize(), "Sizes should be adjusted");
+			assertThrows(FileNotExistException.class, () -> d2.delete("testDoc"), "Cannot delete a file that does not exist");
+			assertThrows(IllegalOperationException.class, () -> d2.delete("."), "Cannot delete '.'");
+			assertThrows(IllegalOperationException.class, () -> d2.delete(".."), "Cannot delete '..'");
+		}catch(Exception e) {
+			assertEquals(1,2,"No exception shall be thrown");
+		}
 	}
 	
 	@Test
@@ -87,7 +102,7 @@ class DirectoryTest {
 			assertThrows(FileNotExistException.class, () -> d2.rename("nosuchfile", "nosuchname"),
 					"Cannot rename a file that does not exist");
 		}catch(Exception e) {
-			assertEquals(1, 1,"No exception should be thrown");
+			assertEquals(1, 2,"No exception shall be thrown");
 		}
 	}
 	
